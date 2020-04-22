15d051f8796a6a9af56161e18967e2b957341e8e
DataSciNAll <jvirtue@microsoft.com>
2020-04-22 09:47:08 -0400

    Lab01_updates

--- setup/lab1_coach/ASA DDL Scripts/05 StoredProcedures.sql
+++ setup/lab1_coach/ASA DDL Scripts/05 StoredProcedures.sql
@@ -966,3 +966,163 @@ BEGIN
     --RETURN 0;
 END;
 GO
+
+/****** Object:  StoredProcedure [Integration].[Configuration_ReseedETL]    Script Date: 4/22/2020 9:22:27 AM ******/
+DROP PROCEDURE [Integration].[Configuration_ReseedETL]
+GO
+/****** Object:  StoredProcedure [Integration].[Configuration_ReseedETL]    Script Date: 4/22/2020 9:22:27 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROC [Integration].[Configuration_ReseedETL] AS
+
+BEGIN
+
+	SET NOCOUNT ON;
+
+
+
+	DECLARE @StartingETLCutoffTime datetime2(7) = '20121231';
+
+	DECLARE @StartOfTime datetime2(7) = '20130101';
+
+	DECLARE @EndOfTime datetime2(7) =  '99991231 23:59:59.9999999';
+
+
+
+	UPDATE Integration.[ETL Cutoff]
+
+		SET [Cutoff Time] = @StartingETLCutoffTime;
+
+
+
+	TRUNCATE TABLE Fact.Movement;
+
+	TRUNCATE TABLE Fact.[Order];
+
+	TRUNCATE TABLE Fact.Purchase;
+
+	TRUNCATE TABLE Fact.Sale;
+
+	TRUNCATE TABLE Fact.[Stock Holding];
+
+	TRUNCATE TABLE Fact.[Transaction];
+
+
+
+	DELETE Dimension.City;
+
+	DELETE Dimension.Customer;
+
+	DELETE Dimension.Employee;
+
+	DELETE Dimension.[Payment Method];
+
+	DELETE Dimension.[Stock Item];
+
+	DELETE Dimension.Supplier;
+
+	DELETE Dimension.[Transaction Type];
+
+
+	SET IDENTITY_INSERT Dimension.City ON;
+    INSERT Dimension.City
+
+        ([City Key], [WWI City ID], City, [State Province], Country, Continent, [Sales Territory], Region, Subregion,
+
+         [Latest Recorded Population], [Valid From], [Valid To], [Lineage Key])
+
+    VALUES
+
+        (0, 0, N'Unknown', N'N/A', N'N/A', N'N/A', N'N/A', N'N/A', N'N/A',
+
+         0, @StartOfTime, @EndOfTime, 0);
+	SET IDENTITY_INSERT Dimension.City OFF;
+
+	SET IDENTITY_INSERT Dimension.Customer ON;
+    INSERT Dimension.Customer
+
+        ([Customer Key], [WWI Customer ID], [Customer], [Bill To Customer], Category, [Buying Group],
+
+         [Primary Contact], [Postal Code], [Valid From], [Valid To], [Lineage Key])
+
+    VALUES
+
+        (0, 0, N'Unknown', N'N/A', N'N/A', N'N/A',
+
+         N'N/A', N'N/A', @StartOfTime, @EndOfTime, 0);
+	SET IDENTITY_INSERT Dimension.Customer OFF;
+
+	SET IDENTITY_INSERT Dimension.Employee ON;
+    INSERT Dimension.Employee
+
+        ([Employee Key], [WWI Employee ID], Employee, [Preferred Name],
+
+         [Is Salesperson], Photo, [Valid From], [Valid To], [Lineage Key])
+
+    VALUES
+
+        (0, 0, N'Unknown', N'N/A',
+         0, NULL, @StartOfTime, @EndOfTime, 0);
+	SET IDENTITY_INSERT Dimension.Employee OFF;
+
+	SET IDENTITY_INSERT Dimension.[Payment Method] ON;
+    INSERT Dimension.[Payment Method]
+
+        ([Payment Method Key], [WWI Payment Method ID], [Payment Method], [Valid From], [Valid To], [Lineage Key])
+
+    VALUES
+
+        (0, 0, N'Unknown', @StartOfTime, @EndOfTime, 0);
+	SET IDENTITY_INSERT Dimension.[Payment Method] OFF;
+
+	SET IDENTITY_INSERT Dimension.[Stock Item] ON;
+    INSERT Dimension.[Stock Item]
+
+        ([Stock Item Key], [WWI Stock Item ID], [Stock Item], Color, [Selling Package], [Buying Package],
+
+         Brand, Size, [Lead Time Days], [Quantity Per Outer], [Is Chiller Stock],
+
+         Barcode, [Tax Rate], [Unit Price], [Recommended Retail Price], [Typical Weight Per Unit],
+
+         Photo, [Valid From], [Valid To], [Lineage Key])
+
+    VALUES
+
+        (0, 0, N'Unknown', N'N/A', N'N/A', N'N/A',
+
+         N'N/A', N'N/A', 0, 0, 0,
+
+         N'N/A', 0, 0, 0, 0,
+
+         NULL, @StartOfTime, @EndOfTime, 0);
+	SET IDENTITY_INSERT Dimension.[Stock Item] OFF
+
+	SET IDENTITY_INSERT Dimension.[Supplier] ON;
+    INSERT Dimension.[Supplier]
+
+        ([Supplier Key], [WWI Supplier ID], Supplier, Category, [Primary Contact], [Supplier Reference],
+
+         [Payment Days], [Postal Code], [Valid From], [Valid To], [Lineage Key])
+
+    VALUES
+
+        (0, 0, N'Unknown', N'N/A', N'N/A', N'N/A',
+
+         0, N'N/A', @StartOfTime, @EndOfTime, 0);
+	SET IDENTITY_INSERT Dimension.[Supplier] OFF
+
+	SET IDENTITY_INSERT Dimension.[Transaction Type] ON;
+    INSERT Dimension.[Transaction Type]
+
+        ([Transaction Type Key], [WWI Transaction Type ID], [Transaction Type], [Valid From], [Valid To], [Lineage Key])
+
+    VALUES
+
+        (0, 0, N'Unknown', @StartOfTime, @EndOfTime, 0);
+	SET IDENTITY_INSERT Dimension.[Transaction Type] OFF;
+
+END;
+GO
+
